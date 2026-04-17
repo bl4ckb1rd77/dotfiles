a433c2704ff45f2edbad99bb24410e68f5b94077
Andre Höfer <bl4ckb1rd1977@googlemail.com>
2026-04-17 09:04:43 +0200

    Init Release - 2026-04-17

---
+++ README.md
@@ -0,0 +1,25 @@
+# dotfiles
+
+---
+
+- **OS**: Arch Linux x86_64
+- **WM**: [Hyprland](https://github.com/hyprwm/Hyprland)
+- **Terminal**: [kitty](https://github.com/kovidgoyal/kitty)
+- **Shell**: Zsh
+  - **Prompt**: [Starship](https://github.com/starship/starship)
+  - **some plugins**:
+- **Noctalia QS Shell**
+- **Login**: sddm Noctalia theme
+- **GTK Theme**: [adw-gtk3-dark]
+- **Icons**: [Papirus-Dark](https://github.com/PapirusDevelopmentTeam/papirus-icon-theme)
+- **Notify**: [swaync](https://github.com/ErikReider/SwayNotificationCenter)
+- **Info**: [fastfetch](https://github.com/fastfetch-cli/fastfetch)
+
+---
+
+![Screenshot](screenshot-1.png "Screenshot Blank Dasktop)
+![Screenshot](screenshot-2.png "Screenshot SysInfo")
+![Screenshot](screenshot-3.png "Screenshot Power Menu")
+![Screenshot](screenshot-4.png "Screenshot Hyprland")
+![Screenshot](screenshot-4.png "Screenshot Notification Center")
+
---
+++ dot_config/exact_hypr/exact_scripts/executable_get_weather.py
@@ -0,0 +1,120 @@
+#!/bin/python3
+
+import json
+import re
+import sys
+from pathlib import Path
+
+import netifaces as ni
+
+#
+# --- Variablen
+#
+wttrcache = Path("/tmp/.wttr.cache")
+ipcache = Path("/tmp/.ip.cache")
+wttrurl = "http://de.wttr.in/"
+ipurl = "http://ipinfo.io"
+wttrformat = "?format=%l:%c:%C:%t:%f:%h:%w"
+expiretime = 600
+ifaceignore = ["virbr", "lo"]
+localif = []
+localip = []
+
+
+#
+# --- functions
+#
+def loadweather():
+    if wttrcache.is_file():
+        with open(wttrcache, "r") as rf:
+            return rf.readline()
+    else:
+        return ""
+
+
+def is_iface_up(iface):
+    addr = ni.ifaddresses(iface)
+    return ni.AF_INET in addr
+
+
+def get_iface_addr(iface):
+    return ni.ifaddresses(iface)[ni.AF_INET][0]["addr"]
+
+
+#
+# --- main
+#
+if len(sys.argv) > 1:
+    if len(sys.argv) < 3:
+        match sys.argv[1]:
+            case "ip" | "-ip" | "--ip":
+                if ipcache.is_file():
+                    with open(ipcache) as filehandler:
+                        ipdata = json.load(filehandler)
+                else:
+                    exit(1)
+                wanip = ipdata["ip"]
+                for iface in ni.interfaces():
+                    if any(x in iface for x in ifaceignore):
+                        continue
+                    if is_iface_up(iface):
+                        ifaddr = get_iface_addr(iface)
+                        localif.append(iface)
+                        localip.append(ifaddr)
+                output = '{"text": "WAN:\t'
+                output += f"{wanip}"
+                for x in range(len(localif)):
+                    output += f"\r{localif[x]}:\t{localip[x]}"
+                output += '"}'
+                print(repr(output).replace("'", ""))
+                exit(0)
+            case "location" | "-location" | "--location":
+                if ipcache.is_file():
+                    with open(ipcache) as filehandler:
+                        ipdata = json.load(filehandler)
+                else:
+                    exit(1)
+                country = ipdata["country"]
+                city = ipdata["city"]
+                if city == "Eschborn":
+                    city = "Köln"
+                print(f"{country}, {city}")
+                exit(0)
+            case "waybar" | "-waybar" | "--waybar":
+                wttr = re.split(":", loadweather())
+                wttrlen = len(wttr)
+                if wttrlen < 4:
+                    waybar = (
+                        '{"text": "offline"'
+                        ', "alt": "", "tooltip": "Service Offline"'
+                        "}"
+                    )
+                else:
+                    waybar = (
+                        '{"text": '
+                        f'"{wttr[0]}{wttr[3]}"'
+                        ', "alt": ""'
+                        ', "tooltip": '
+                        f'"{wttr[0]}\t{wttr[1]} {wttr[2]}\r'
+                        f"\t{wttr[3]} ({wttr[4]})\r"
+                        f'\t{wttr[5]} {wttr[6]}"'
+                        "}"
+                    )
+                print(repr(waybar).replace("'", ""))
+                exit(0)
+            case "weather" | "-weather" | "--weather":
+                wttr = re.split(":", loadweather())
+                wttrlen = len(wttr)
+                if wttrlen < 4:
+                    print("Service Offline")
+                else:
+                    print(f"{wttr[0]}{wttr[1]} {wttr[2]}")
+                exit(0)
+            case _:
+                print("ups")
+                exit(1)
+    else:
+        print("to many params")
+        exit(2)
+else:
+    print("no param")
